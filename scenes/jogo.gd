3327641a84cca9508ebc11acf9903a4d68abd60f
RodCrvlh <digaorrvcc@gmail.com>
2025-09-07 18:36:00 -0300

    A peca anda e o jogo nao crasha

--- scenes/jogo.gd
+++ scenes/jogo.gd
@@ -4,12 +4,21 @@ extends Node2D
 @onready var peca: Sprite2D = $Peca
 @onready var ferrovia: Marker2D = $Ferrovia
 @export var espacos: Array[Node]
-var posicao: int = 14
+var posicao: int = 0
+var numero_espacos: int 
+
+func _ready() -> void:
+	numero_espacos = espacos.size()
+	print(numero_espacos)
 
 func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_click"):
+	if Input.is_action_just_pressed("ui_click") and posicao <= (numero_espacos-1):
 		var tween = create_tween()
 		tween.tween_property(peca, "position", espacos[posicao].position, 1)
-			
+		posicao += 1
+	elif posicao == numero_espacos:
+		posicao =0
+		
+
    
 	
