caea20dab49859baece5394debbaa9f8eca110b3
RodCrvlh <digaorrvcc@gmail.com>
2025-09-07 17:12:11 -0300

    Mudança do tabuleiro e adicionando marker2d dos Espaco

--- scenes/jogo.gd
+++ scenes/jogo.gd
@@ -4,9 +4,12 @@ extends Node2D
 @onready var peca: Sprite2D = $Peca
 @onready var ferrovia: Marker2D = $Ferrovia
 @export var espacos: Array[Node]
+var posicao: int = 14
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_click"):
 		var tween = create_tween()
-		tween.tween_property(peca, "position", ferrovia.position, 1)
+		tween.tween_property(peca, "position", espacos[posicao].position, 1)
+			
+   
 	
